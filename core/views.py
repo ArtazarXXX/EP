ea3305e3cc3248c8468bd139b49e0579e30b8a40
ArtazarXXX <artemgribanov886@gmail.com>
2021-05-14 08:43:49 +0300

    add RestAPI

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import CitySerializer
+from .models import City
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
